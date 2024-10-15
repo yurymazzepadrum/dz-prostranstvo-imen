3f3c2b5f2dddf1ef0185b5e954abaff75df07117
yurymazzepadrum <y9853353903@yandex.ru>
2024-10-15 15:47:25 +0300

    Add files via upload

--- dz prostranstvo imen.py
+++ dz prostranstvo imen.py
@@ -1,17 +1,19 @@
 calls = 0
 
+
+
 def count_calls():
     global calls
     calls += 1
+
 def string_info(string):
     count_calls()
     return (len(string), string.upper(), string.lower())
 
 def is_contains(string,  list_to_search):
-    if string == list_to_search:
-        return True
-    else:
-        return False
+    count_calls()
+    return string.lower() in [i.lower() for i in list_to_search] # 
+
 
 
 
